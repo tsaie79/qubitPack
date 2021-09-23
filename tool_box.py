48eb0067d09fb51e9f71d0fde8369d356b201e6c
jeng-yuantsai <tsaie79@gmail.com>
2021-09-23 13:03:05 -0400

    update

--- tool_box.py
+++ tool_box.py
@@ -482,10 +482,11 @@ def get_lowest_unocc_band_idx(task_id, db_obj, nbands, prevent_JT=True, second_e
 def phonopy_structure(orig_st):
     from subprocess import call, check_output
     import shutil
+
     os.makedirs("standardize_st", exist_ok=True)
     os.chdir("standardize_st")
     orig_st.to("poscar", "POSCAR")
-    call("phonopy --symmetry --tolerance 0.01 -c POSCAR".split(" "), shell=True)
+    call("phonopy --symmetry --tolerance 0.01 -c ~/standardize_st/POSCAR".split(" "), shell=True)
     std_st = Structure.from_file("PPOSCAR")
     std_st.to("poscar", "POSCAR")
     pos2aBR_out = check_output(["pos2aBR"], universal_newlines=True).split("\n")
