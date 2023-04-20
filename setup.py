b66dad4c49e1eb7e12f39815f39d9a6462fd1b9e
jeng-yuantsai <tsaie79@gmail.com>
2023-04-19 23:08:28 -0400

    update

---
+++ setup.py
@@ -0,0 +1,13 @@
+# add a setup file to the package directory
+
+from setuptools import setup, find_packages
+
+setup(
+    name="qubitPack",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "-e git+https://github.com/tsaie79/atomate.git@c2b16d05a68999b4adc88afa2802804132c691b3#egg=atomate",
+        ]
+    )
+
