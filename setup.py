ffb4bde0f955386ba23992e4d54392c1d8257d2e
jeng-yuantsai <tsaie79@gmail.com>
2023-04-20 22:10:55 -0400

    update

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "smoqe@git+https://github.com/dangunter/smoqe.git@master#egg=smoqe"
-        "pymatgen_db@git+https://github.com/materialsproject/pymatgen-db.git@c3271276c2ef26dc98ccc86634405a04cd677395"
-        "#egg=pymatgen_db",
-        "pymatgen_diffusion@git+https://github.com/materialsvirtuallab/pymatgen-analysis-diffusion.git"
-        "@51d84ea1fd034941baa22d4b1b8610a4cc6fb801#egg=pymatgen_diffusion",
-        "ase@git+ssh://git@github.com/rosswhitfield/ase.git@07de35654601ddbb2b23a4e7df7091696b0af108#egg=ase",
-        "pycdt@git+https://github.com/tsaie79/pycdt.git@aaa8249ed4dace846ca49299b83aa831230b70f8#egg=pycdt",
+        # "smoqe@git+https://github.com/dangunter/smoqe.git@master#egg=smoqe"
+        # "pymatgen_db@git+https://github.com/materialsproject/pymatgen-db.git@c3271276c2ef26dc98ccc86634405a04cd677395"
+        # "#egg=pymatgen_db",
+        # "pymatgen_diffusion@git+https://github.com/materialsvirtuallab/pymatgen-analysis-diffusion.git"
+        # "@51d84ea1fd034941baa22d4b1b8610a4cc6fb801#egg=pymatgen_diffusion",
+        # "ase@git+ssh://git@github.com/rosswhitfield/ase.git@07de35654601ddbb2b23a4e7df7091696b0af108#egg=ase",
+        # "pycdt@git+https://github.com/tsaie79/pycdt.git@aaa8249ed4dace846ca49299b83aa831230b70f8#egg=pycdt",
         "pymatgen@git+https://github.com/tsaie79/pymatgen.git@Jcustom_plotter#egg=pymatgen",
-        "atomate@git+https://github.com/tsaie79/atomate.git@c2b16d05a68999b4adc88afa2802804132c691b3#egg=atomate",
-        "FireWorks==1.9.6",
-        "custodian==2020.4.27",
+        # "atomate@git+https://github.com/tsaie79/atomate.git@c2b16d05a68999b4adc88afa2802804132c691b3#egg=atomate",
+        # "FireWorks==1.9.6",
+        # "custodian==2020.4.27",
 
     ],
 )
