264ba124510e408ea197fb54ff5d5f04f304cd29
jeng-yuantsai <tsaie79@gmail.com>
2023-04-24 01:14:23 -0400

    update

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         "pymatgen_diffusion@git+https://github.com/materialsvirtuallab/pymatgen-analysis-diffusion.git"
         "@51d84ea1fd034941baa22d4b1b8610a4cc6fb801#egg=pymatgen_diffusion",
         "ase@git+ssh://git@github.com/rosswhitfield/ase.git@07de35654601ddbb2b23a4e7df7091696b0af108#egg=ase",
-        "pycdt@git+https://github.com/tsaie79/pycdt.git@aaa8249ed4dace846ca49299b83aa831230b70f8#egg=pycdt",
+        "pycdt@git+https://github.com/tsaie79/pycdt.git@master#egg=pycdt",
         "pymatgen@git+https://github.com/tsaie79/pymatgen.git@master#egg=pymatgen",
         "atomate@git+https://github.com/tsaie79/atomate.git@master#egg=atomate",
         "FireWorks==1.9.6",
